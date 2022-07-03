4e5adda0f0a420eb36fb52ec451dd705291671ad
Betsy <betsy.griffith12@gmail.com>
2022-07-02 21:47:27 -0400

    test passed for creating employee name and email

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,3 +1,9 @@
-// const Employee = require("../lib/Employee");
+const Employee = require("../lib/Employee")
 
-// test("creates an Employee object")
+test('creates an Employee object', () => {
+    const employee = new Employee('');
+
+    expect(employee.name).toBe('')
+    expect(employee.email).toBe('');
+});
+
