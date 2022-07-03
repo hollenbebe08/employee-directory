a41e265c1cb37a7b191f36f3474a9c05bb93aa88
Betsy <betsy.griffith12@gmail.com>
2022-07-03 09:36:34 -0400

    created getid() prototype that passed the test in the employee.test.js file

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,15 @@
 const Employee = require("../lib/Employee")
 
 test('creates an Employee object', () => {
-    const employee = new Employee('');
+    const employee = new Employee();
 
-    expect(employee.name).toBe('')
-    expect(employee.email).toBe('');
+    expect(typeof(employee)).toBe('object')
 });
+
+test('creates an Employee id', () => {
+    const employee = new Employee();
+
+    //the id should be an array
+    expect(employee.id).toEqual(expect.any(Array));
+})
 
