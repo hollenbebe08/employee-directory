05a027eb9bea01b6fcceb7043b43124a4f54d0c8
Betsy <betsy.griffith12@gmail.com>
2022-07-03 15:53:34 -0400

    inital Employee Class has been created and passed all the tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -6,10 +6,32 @@ test('creates an Employee object', () => {
     expect(typeof(employee)).toBe('object')
 });
 
+test('creates an Employee name', () => {
+    const employee = new Employee();
+
+    //the name should be a string
+    expect(employee.name).toEqual(expect.any(String));
+})
+
 test('creates an Employee id', () => {
     const employee = new Employee();
 
     //the id should be an array
     expect(employee.id).toEqual(expect.any(Array));
+})
+
+test('creates an Employee email', () => {
+    const employee = new Employee();
+
+    //the email should be a string
+    expect(employee.email).toEqual(expect.any(String));
+})
+
+test('creates an employee role', () => {
+    const role = 'Employee';
+    const employee = new Employee();
+
+    //the role should be an array
+    expect(employee.role).toEqual(expect.any(Array));
 })
 
