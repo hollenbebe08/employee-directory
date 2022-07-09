10e4f9a69128e32d6b871c7ac130766f65a54b0b
Betsy <betsy.griffith12@gmail.com>
2022-07-08 21:19:48 -0400

    implemented Bulma. Added card styling for Manager and Engineer in output-template.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generatePage = require('./lib/Manager-questions');
+const generatePage = require('./lib/output-template');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
